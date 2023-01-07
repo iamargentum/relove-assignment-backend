a94cf52171449bad7787c23b6322f10c2bef05c7
iamargentum <rajat.rj97@gmail.com>
2023-01-07 21:39:05 +0530

    Introduce event for submitting answers

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, request
 from flask_socketio import SocketIO
 from utils import createQuestionAndGetAnswer
-from serialize import serializeConnectedClientList
+from serialize import serializeConnectedClientList, serializeConnectedClient
 
 OPERATION_CHOICES = [
     "ADD",
@@ -75,6 +75,51 @@ def clientDisconnected():
         cursor = dbConn.cursor()
         cursor.execute(f"DELETE FROM connected_clients where sid='{request.sid}'")
         dbConn.commit()
+
+@socketio.on("submitAnswer")
+def answerSubmitted(data):
+    global submittedAnswers
+    print("data is ", data)
+    print("sent by sid", request.sid)
+    data["sid"] = request.sid
+    submittedAnswers.append(data)
+    with sqlite3.connect("mathApp.db") as dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute(f"UPDATE connected_clients SET ready={True} WHERE sid='{request.sid}'")
+        dbConn.commit()
+        result = cursor.execute(f"SELECT * FROM connected_clients")
+        result = result.fetchall()
+        listOfConnectedClients = serializeConnectedClientList(result)
+        if len(listOfConnectedClients) == len(submittedAnswers):
+            print("inside list of clients is equal to submitted answers")
+            fastestAnswerIndex = 0
+            leastTime = submittedAnswers[0]["time"]
+            for i in range(1, len(submittedAnswers)):
+                answer = submittedAnswers[i]
+                if answer["time"] < leastTime:
+                    fastestAnswerIndex = i
+            winnerSid = submittedAnswers[fastestAnswerIndex]["sid"]
+            result = cursor.execute(f"SELECT * FROM connected_clients WHERE sid='{winnerSid}'")
+            result = result.fetchone()
+            winner = serializeConnectedClient(result)
+
+            print("winner is ", winner)
+
+            global operands
+            global operation
+            global currentAnswer
+            
+            operands = {
+                "first": 0,
+                "second": 0
+            }
+            operation = ""
+            currentAnswer = 0
+            submittedAnswers = []
+
+            cursor.execute("UPDATE connected_clients SET ready=false")
+
+            socketio.emit("winner", winner)
 
 if __name__ == '__main__':
     socketio.run(app)
