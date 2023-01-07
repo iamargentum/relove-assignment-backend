67e16f25543e28cf83c3ffc201a1550baea4e2d0
iamargentum <rajat.rj97@gmail.com>
2023-01-07 18:07:47 +0530

    Remove ORM and use `sqlite3`

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
+import sqlite3
 from flask import Flask, request
-from flask_migrate import Migrate
 from flask_socketio import SocketIO
-from flask_sqlalchemy import SQLAlchemy
-from serialize import serializeConnectedClient, serializeConnectedClientList
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mathApp.db"
-socketio = SocketIO(app, cors_allowed_origins="*")
-db = SQLAlchemy(app)
-db.init_app(app)
-
-migrate = Migrate(app, db)
 
 OPERATION_CHOICES = [
     "ADD",
@@ -24,56 +14,38 @@ operands = {
     "second": 0
 }
 
-operation = []
+operation = ""
 
-with app.app_context():
-    db.create_all()
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on("connect")
 def clientConnected():
-    from models import ConnectedClient
-    print("request is ", request.sid)
-    connectedClient = ConnectedClient(
-        sid=request.sid,
-        name=""
-    )
-    db.session.add(connectedClient)
-    db.session.commit()
-
+    with sqlite3.connect("mathApp.db") as dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute(f"INSERT INTO connected_clients (sid, name, ready) VALUES ('{request.sid}', '', {False})")
+        dbConn.commit()
 
 @socketio.on("setName")
 def setClientName(data):
-    from models import ConnectedClient
-    connectedClient = ConnectedClient.query.filter_by(sid=request.sid).first()
-    connectedClient.name = data
-    db.session.commit()
-    connectedClientList = ConnectedClient.query.all()
-    socketio.emit("updateUsersList", serializeConnectedClientList(connectedClientList), broadcast=True)
+    with sqlite3.connect("mathApp.db") as dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute(f"UPDATE connected_clients SET name='{data}' WHERE sid='{request.sid}'")
+        dbConn.commit()
 
 @socketio.on("ready")
-def setClientName():
-    from models import ConnectedClient
-    connectedClient = ConnectedClient.query.filter_by(sid=request.sid).first()
-    connectedClient.ready = True
-    db.session.commit()
-    db.session.refresh(connectedClient)
-    clientsNotReady = ConnectedClient.query.filter_by(ready=False).all()
-    print("clients that are not ready - ", clientsNotReady)
+def clientReady():
+    with sqlite3.connect("mathApp.db") as dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute(f"UPDATE connected_clients SET ready={True} WHERE sid='{request.sid}'")
+        dbConn.commit()
 
 @socketio.on("disconnect")
-def disconnect():
-    print("disconnect triggered")
-    print("request.sid is ", request.sid)
-    from models import ConnectedClient
-    db.session.execute('DELETE FROM connected_client WHERE sid = :val', {'val': request.sid})
-    db.session.commit()
-    connectedClientList = ConnectedClient.query.all()
-    socketio.emit("updateUsersList", serializeConnectedClientList(connectedClientList), broadcast=True)
+def clientDisconnected():
+    with sqlite3.connect("mathApp.db") as dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute(f"DELETE FROM connected_clients where sid='{request.sid}'")
+        dbConn.commit()
 
-@socketio.on("submitAnswer")
-def userSubmitted(data):
-    pass
-
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True)
+if __name__ == '__main__':
+    socketio.run(app)
